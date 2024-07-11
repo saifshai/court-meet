2a38b1ee885bfa1dfba233a18208ab98076a5cb1
Saif <sshai072@uottawa.ca>
2024-07-11 19:31:29 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
+import SearchResults from './components/SearchResults';
+import MeetupDetails from './components/MeetupDetails';
+import CreateMeetup from './components/CreateMeetup';
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
+  <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Court Meet</h1>
+        <nav>
+          <Link to="/">Home</Link> | 
+          <Link to="/search">Find Meetups</Link> | 
+          <Link to="/create">Create Meetup</Link>
+        </nav>
       </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={SearchResults} />
+          <Route path="/details" component={MeetupDetails} />
+          <Route path="/create" component={CreateMeetup} />
+        </Switch>
+      </main>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
+
